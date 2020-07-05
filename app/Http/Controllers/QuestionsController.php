9e4e0b4a8008183507ffee7ec86a36f7126f4095
Sanjay Rohra <rohra470@gmail.com>
2020-07-05 20:31:15 +0530

    single question shown

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -13,17 +13,21 @@ class QuestionsController extends Controller
     public function __construct()
     {
         $this->middleware(['auth'])->except(['index','show']);
+
     }
 
     /**
+     *
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
+        //
         $questions = Question::with('owner')->latest()->paginate(10);
-        return view('questions.index', compact('questions'));
+        return view('questions.index',compact('questions'));
+
     }
 
     /**
@@ -33,55 +37,56 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        // app('debugbar').disable();
         return view('questions.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(CreateQuestionRequestAlias $request)
+    public function store(CreateQuestionRequest $request)
     {
         auth()->user()->questions()->create([
             'title' => $request->title,
             'body' => $request->body,
         ]);
-        session()->flash('success', 'Question has been added successfully');
+
+        session()->flash('success','Question has been added successfully');
         return redirect(route('questions.index'));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param int $id
+     * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Question $question)
     {
-        //
+        $question->increment('views_count');
+        return view('questions.show',compact('question'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param Question $question
+     * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
     public function edit(Question $question)
     {
-        return view('questions.edit', compact('question'));
-
+        return view('questions.edit',compact("question"));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param UpdateQuestionRequest $request
-     * @param Question $question
-     * @return void
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Question  $question
+     * @return \Illuminate\Http\Response
      */
     public function update(UpdateQuestionRequest $request, Question $question)
     {
@@ -89,22 +94,21 @@ class QuestionsController extends Controller
             "title" => $request->title,
             "body" => $request->body,
         ]);
-        session()->flash('success', 'Question has been modified successfully');
+
+        session()->flash('success','Question has been Modified successfully');
         return redirect(route('questions.index'));
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param Question $question
-     * @return void
-     * @throws \Exception
+     * @param  \App\Question  $question
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Question $question)
     {
         $question->delete();
-        session()->flash('success', 'Question has been deleted successfully');
+        session()->flash('success','Question has been deleted successfully');
         return redirect(route('questions.index'));
-
     }
 }
