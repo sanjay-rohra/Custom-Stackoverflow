b0324fe84d14ae4ac66d5d6b8c105f3dda08781e
Sanjay Rohra <rohra470@gmail.com>
2020-07-06 20:11:47 +0530

    Answers can be Edited

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Answer;
 use App\Http\Requests\Answers\CreateAnswerRequest;
 use App\Question;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -14,7 +15,7 @@ class AnswersController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return Response
+     * @return void
      */
     public function create()
     {
@@ -24,7 +25,8 @@ class AnswersController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Question $question
+     * @param CreateAnswerRequest $request
      * @return Response
      */
     public function store(Question $question, CreateAnswerRequest $request)
@@ -35,14 +37,14 @@ class AnswersController extends Controller
             'user_id' => auth()->id(),
         ]);
 
-        session()->flash('success','Answer has been added successfully');
+        session()->flash('success', 'Answer has been added successfully');
         return redirect($question->url);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Answer  $answer
+     * @param Answer $answer
      * @return Response
      */
     public function show(Answer $answer)
@@ -53,31 +55,41 @@ class AnswersController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Answer  $answer
-     * @return Response
+     * @param Question $question
+     * @param Answer $answer
+     * @return void
+     * @throws AuthorizationException
      */
-    public function edit(Answer $answer)
+    public function edit(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update',$answer);
+        return view('answers.edit',compact(['question','answer']));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Answer  $answer
-     * @return Response
+     * @param Request $request
+     * @param Answer $answer
+     * @param Question $question
+     * @return void
+     * @throws AuthorizationException
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question,Answer $answer)
     {
-        //
+        $this->authorize('update',$answer);
+        $answer->update([
+            'body'=>$request->body
+        ]);
+        session()->flash('success','Answer has been updated successfully');
+        return redirect($question->url);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Answer  $answer
-     * @return Response
+     * @param Answer $answer
+     * @return void
      */
     public function destroy(Answer $answer)
     {
